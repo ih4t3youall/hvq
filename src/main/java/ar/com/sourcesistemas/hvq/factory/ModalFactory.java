a5311acd4778e61002159a6f81f94b4535cd8d49
lequerica <lequerica@Martins-MacBook-Pro.local>
2018-05-22 16:31:54 -0300

    added autocomplete for the products

--- src/main/java/ar/com/sourcesistemas/hvq/factory/ModalFactory.java
+++ src/main/java/ar/com/sourcesistemas/hvq/factory/ModalFactory.java
@@ -12,6 +12,19 @@ public class ModalFactory {
 	public ModelAndView getModal(String modalName) {
 
 		ModelAndView mav = new ModelAndView(MODAL_FOLDER + SEPARATOR + modalName);
+		String[] values = new String[6];
+		values[0] = "martin";
+		values[1] = "florencia";
+		values[2] = "paradise";
+		values[3] = "carla";
+		values[4] = "juan";
+		values[5] = "lequerica";
+
+		for (String nose : values) {
+			System.out.println(nose);
+		}
+
+		mav.addObject("countries", values);
 		return mav;
 	}
 
