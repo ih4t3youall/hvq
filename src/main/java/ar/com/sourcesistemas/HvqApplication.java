de3fd9d8c308007c1334938e4c6bc6b7b8c06faa
Martin Lequerica <mlequerica@viridian.local>
2018-05-30 11:53:04 -0300

    now ajax is working

--- src/main/java/ar/com/sourcesistemas/HvqApplication.java
+++ src/main/java/ar/com/sourcesistemas/HvqApplication.java
@@ -2,11 +2,23 @@ package ar.com.sourcesistemas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class HvqApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HvqApplication.class, args);
+	}
+
+	@Configuration
+	public static class PathMatchingConfigurationAdapter extends WebMvcConfigurerAdapter {
+	 
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	configurer.favorPathExtension(false);
+	}
 	}
 }
--- src/main/java/ar/com/sourcesistemas/controller/ProductController.java
+++ src/main/java/ar/com/sourcesistemas/controller/ProductController.java
@@ -4,28 +4,53 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import ar.com.sourcesistemas.model.Product;
 
 @Controller
 public class ProductController {
 
 	private Map<String, Integer> prices = new HashMap<String, Integer>();
 
-	@RequestMapping(value = "/user/getProductPriceByName", method = RequestMethod.GET)
-	public @ResponseBody String getProructPriceByName(String productName) {
+	@GetMapping("/user/getProductPriceByName")
+	@ResponseBody 
+	public String getProructPriceByName(String productName) {
 		init();
 		return prices.get(productName).toString();
-
 	}
 
+	@PostMapping(value = "/user/getProductById")
+	@ResponseBody 
+	public Product getPoruductById2(@RequestBody String id) {
+		System.out.println("id: " + id);
+		Product product = new Product();
+		product.setName("productName");
+		product.setPrice(10);
+
+		return product;
+	}
+	
+	@GetMapping(value = "/user/getProductById")
+	@ResponseBody 
+	public Product getPoruductById(String id) {
+		System.out.println("id: " + id);
+		Product product = new Product();
+		product.setName("productName");
+		product.setPrice(10);
+
+		return product;
+	}
+
+
 	public void init() {
-
 		String[] values = { "martin", "florencia", "paradise", "carla", "juan", "lequerica" };
-
 		for (int i = 0; i < values.length; i++) {
 			prices.put(values[i], i);
 		}
 	}
 }
+
