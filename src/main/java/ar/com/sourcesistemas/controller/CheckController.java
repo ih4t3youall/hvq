eeda4430413bb02dd86c202bafc0aea32f91f651
Martin Lequerica <mlequerica@viridian.local>
2018-06-07 12:20:31 -0300

    adding bootstrap

--- src/main/java/ar/com/sourcesistemas/controller/CheckController.java
+++ src/main/java/ar/com/sourcesistemas/controller/CheckController.java
@@ -4,11 +4,13 @@ import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.com.sourcesistemas.dto.CheckDTO;
@@ -39,10 +41,10 @@ public class CheckController {
 	}
 
 	@GetMapping("/user/clearAll")
-	public String clearAll() {
+	@ResponseStatus(value = HttpStatus.OK)
+	public void clearAll() {
 		userCheck.clear();
 
-		return "200ok";
 	}
 
 }
