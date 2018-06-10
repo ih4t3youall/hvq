2efc37b6a4370c9b0cebab7172de1315bb186cd8
lequerica <lequerica@Martins-MacBook-Pro.local>
2018-06-09 22:02:55 -0300

    resolve merge

--- src/main/resources/static/js/sourcesistemas/check.js
+++ src/main/resources/static/js/sourcesistemas/check.js
@@ -1,56 +1,62 @@
 function facturar() {
 
+	if ($(".productRow").length == 0) {
+
+		$.alert({
+			backgroundDismiss : true,
+			content : 'No Hay ningun producto!',
+		});
+	} else {
 		cashDebit();
-
 	}
+}
 
-	function doCheck(payWith) {
-		var productDTO = [];
-		$('.productRow').each(function(index, item) {
-			var product = new Object();
-			product.price = $(item).find('.price').html();
-			product.name = $(item).find('.name').html();
-			product.qty = $(item).find('.qty').html();
-			productDTO.push(product);
-		});
+function doCheck(payWith) {
+	var productDTO = [];
+	$('.productRow').each(function(index, item) {
+		var product = new Object();
+		product.price = $(item).find('.price').html();
+		product.name = $(item).find('.name').html();
+		product.qty = $(item).find('.qty').html();
+		productDTO.push(product);
+	});
 
-		var checkDTO = new Object();
-		checkDTO.payWith = payWith;
-		checkDTO.productsDTO = productDTO;
-		$.ajax({
-			type : "POST",
-			url : "doCheck.htm",
-			data : JSON.stringify(checkDTO),
-			contentType : "application/json",
-			success : function(response) {
-				console.log(response);
-				window.location.replace("getCheck");
+	var checkDTO = new Object();
+	checkDTO.payWith = payWith;
+	checkDTO.productsDTO = productDTO;
+	$.ajax({
+		type : "POST",
+		url : "doCheck.htm",
+		data : JSON.stringify(checkDTO),
+		contentType : "application/json",
+		success : function(response) {
+			console.log(response);
+			window.location.replace("getCheck");
+		},
+		error : function(e) {
+			console.log(e);
+		}
+	});
+}
+
+function cashDebit() {
+	$.confirm({
+		title : 'Confirmacion!',
+		content : 'Seleccione con que abonar!',
+		backgroundDismissAnimation : 'glow',
+		buttons : {
+			Debito : function() {
+				doCheck("debito");
 			},
-			error : function(e) {
-				console.log(e);
-			}
-		});
-	}
+			Efectivo : function() {
+				doCheck("efectivo");
+			},
+			Credito : function() {
+				doCheck("credito");
+			},
+			Cancelar : function() {
 
-	function cashDebit() {
-		$.confirm({
-			title : 'Confirmacion!',
-			content : 'Seleccione con que abonar!',
-			backgroundDismissAnimation : 'glow',
-			buttons : {
-				Debito : function() {
-					doCheck("debito");
-				},
-				Efectivo : function() {
-					doCheck("efectivo");
-				},
-				Credito : function() {
-					doCheck("credito");
-				},
-				Cancelar : function() {
-
-				}
 			}
-		});
-	}
-	
+		}
+	});
+}
